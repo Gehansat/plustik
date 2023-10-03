e8f15e417c984e5db4351711b8c0e60b5b2e35cd
IT21108990 <IT21108990@my.sliit.lk>
2023-10-03 19:15:09 +0530

    register done, Google sign in first step
    few problems:
    if enters wrong input to confirm password it will not show the error msg,
    
    displayName
    
    -------------------
    
    moved some files downloaded from google

--- lib/components/login/sign_in_out_button.dart
+++ lib/components/login/sign_in_out_button.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class SignInButton extends StatelessWidget {
   final Function()? onTap;
+  final String text;
 
   const SignInButton({
-    super.key,
-    required this.onTap
-  });
+    super.key, 
+    required this.onTap,
+    required this.text,
+    });
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +20,9 @@ class SignInButton extends StatelessWidget {
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               color: const Color(0xff00B140)),
-          child: const Center(
-            child: Text("SIGN IN",
-                style: TextStyle(
+          child: Center(
+            child: Text(text,
+                style: const TextStyle(
                     color: Colors.white,
                     fontSize: 16,
                     fontWeight: FontWeight.w600)),
