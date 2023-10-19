efbfcfede417cca26e557d1a560b0062ba7437d8
Ashani Rasangika Dewmini <it21016820@my.sliit.lk>
2023-10-19 14:41:44 +0530

    create navigations to reminder and notify

---
+++ lib/pages/notifications/notify_type_pg.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotifyTypePage extends StatelessWidget {
+  const NotifyTypePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
