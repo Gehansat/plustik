d551a1f07c9365abe3937808b65a86e8ef5e2125
Gehan Sathushka <it21102196@my.sliit.lk>
2023-10-27 09:07:35 +0530

    Create appointment

--- lib/pages/appointments/appointment.dart
+++ lib/pages/appointments/appointment.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:plustik/pages/appointments/successpage.dart';
+
 
 class AddAppointmentPage extends StatefulWidget {
   @override
-  _AddAppointmentPage createState() => _AddAppointmentPage();
+  _AddAppointmentPageState createState() => _AddAppointmentPageState();
 }
 
-class _AddAppointmentPage extends State<AddAppointmentPage> {
+class _AddAppointmentPageState extends State<AddAppointmentPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController dateController = TextEditingController();
   final TextEditingController timeController = TextEditingController();
@@ -17,129 +19,252 @@ class _AddAppointmentPage extends State<AddAppointmentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            padding: EdgeInsets.all(30.0),
-
-            children: [
-              const SizedBox(height: 40),
-              const Center(
-                child: Text(
-                  'Add an Appointment',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        backgroundColor: Color(0xff00B140),
+        title: Text('Make Your Appointment'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/logo.png', // Replace with your image asset
+                    scale: 1.7,
                   ),
-                ),
-              ),
-              const SizedBox(height: 50),
-              
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: dateController,
-                decoration: const InputDecoration(
-                  labelText: 'Date',
-                  hintText: 'YYYY-MM-DD',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a date';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: timeController,
-                decoration: const InputDecoration(
-                  labelText: 'Time',
-                  hintText: 'HH:MM',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a time';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: contactnoController,
-                decoration: InputDecoration(labelText: 'Contact no'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a Contact No';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an Email';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 25),
-              TextFormField(
-                controller: instructionsController,
-                decoration: InputDecoration(labelText: 'Instructions'),
-              ),
-              const SizedBox(height: 40),
-              
-              ElevatedButton(
-
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xff00B140),
-                  onPrimary: Colors.white,
-                  minimumSize: const Size(80, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0), // Adjust the value as needed
+                  SizedBox(height: 10),
+                  Text(
+                    'Make Your Appointment',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                ),
-                onPressed: () async {
-                  print("hello");
-                  if (_formKey.currentState!.validate()) {
-                    await addAppointmentToFirebase();
-                  }
-                  print("hello");
-                },
-                child: Text('Create Appointment'),
+                ],
               ),
-            ],
-          ),
+            ),
+            Form(
+              key: _formKey,
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    buildTextFormField(
+                      label: 'Date',
+                      controller: dateController,
+                      hint: 'Date (YYYY-MM-DD)',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a date';
+                        }
+                        if (!isValidDate(value)) {
+                          return 'Invalid date format (YYYY-MM-DD)';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    buildTextFormField(
+                      label: 'Time',
+                      controller: timeController,
+                      hint: 'Time (HH:MM)',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a time';
+                        }
+                        if (!isValidTime(value)) {
+                          return 'Invalid time format (HH:MM)';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    buildTextFormField(
+                      label: 'Contact Number',
+                      controller: contactnoController,
+                      hint: 'Contact Number',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a contact number';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    buildTextFormField(
+                      label: 'Email',
+                      controller: emailController,
+                      hint: 'Email',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an email';
+                        }
+                        if (!isValidEmail(value)) {
+                          return 'Invalid email format';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    buildTextFormField(
+                      label: 'Instructions',
+                      controller: instructionsController,
+                      hint: 'Instructions',
+                      validator: (value) {
+                        // You can add custom validation for instructions here if needed.
+                        return null; // No validation in this example.
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    GestureDetector(
+                      onTap: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await addAppointmentDetailsToFirebase();
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => Successpage(
+                                date: dateController.text,
+                                time: timeController.text,
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      child: Container(
+                        width: 200,
+                        height: 40,
+                        child: Card(
+                          color: Color(0xff00B140), // Background color
+                          elevation: 4,
+                          child: Center(
+                            child: Text(
+                              'Confirm',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-
-  Future<void> addAppointmentToFirebase() async {
-    final appointmentsCollection = FirebaseFirestore.instance.collection('Appointments');
-    final appointmentsDateTime = DateTime.parse("${dateController.text} ${timeController.text}");
-    await appointmentsCollection.add({
-      'DateTime': appointmentsDateTime,
-      'Contactno': contactnoController,
-      'Email': emailController.text,
-      'Instructions': instructionsController.text,
-    });
-
-    // Show a success message or navigate to another page
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        backgroundColor: Color(0xff00B140),
-        content: Text('Appointment added successfully',
-        style: TextStyle(
-          fontSize: 17,
-          // fontWeight: FontWeight.bold,
-          ),
+  Widget buildTextFormField({
+    required String label,
+    required TextEditingController controller,
+    required String hint,
+    String? Function(String?)? validator,
+  }) {
+                               
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.grey),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue),
+          borderRadius: BorderRadius.circular(10),
         ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      validator: validator,
+    );
+  }
+
+  bool isValidDate(String value) {
+    final pattern = r'^\d{4}-\d{2}-\d{2}$';
+    final regExp = RegExp(pattern);
+    return regExp.hasMatch(value);
+  }
+
+  bool isValidTime(String value) {
+    final pattern = r'^\d{2}:\d{2}$';
+    final regExp = RegExp(pattern);
+    return regExp.hasMatch(value);
+  }
+
+  bool isValidEmail(String value) {
+    final pattern = r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-z]{2,})$';
+    final regExp = RegExp(pattern);
+    return regExp.hasMatch(value);
+  }
+
+  Future<void> addAppointmentDetailsToFirebase() async {
+    final appointmentDetailsCollection =
+        FirebaseFirestore.instance.collection('AppointmentDetails');
+    final details = AppointmentDetails(
+      date: dateController.text,
+      time: timeController.text,
+      contactno: contactnoController.text,
+      email: emailController.text,
+      instructions: instructionsController.text,
+    );
+
+    await appointmentDetailsCollection.add(details.toMap());
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Appointment Created Successfully'),
       ),
     );
   }
 }
+
+class AppointmentDetails {
+  final String date;
+  final String time;
+  final String contactno;
+  final String email;
+  final String instructions;
+
+  AppointmentDetails({
+    required this.date,
+    required this.time,
+    required this.contactno,
+    required this.email,
+    required this.instructions,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'date': date,
+      'time': time,
+      'contactno': contactno,
+      'email': email,
+      'instructions': instructions,
+    };
+  }
+}
+
+// class Successpage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Appointment Success'),
+//         backgroundColor: Color(0xff00B140),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'Appointment has been created successfully!',
+//               style: TextStyle(fontSize: 18),
+//               textAlign: TextAlign.center,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/pages/appointments/successpage.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class Successpage extends StatelessWidget {
+  final String date; // Add date parameter to get from the database
+  final String time; // Add time parameter to get from the database
+
+  Successpage({Key? key, required this.date, required this.time}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Date: $date');
+    print('Time: $time');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Success Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Align content at the center and space evenly
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20), // Add some space at the top
+              Image.asset(
+                'assets/logo.png', // Your logo image asset
+                // height: 150,
+                scale: 1.5,
+              ),
+              const SizedBox(height: 30),
+              Image.asset(
+                'assets/success.png', // Your success image asset
+                height: 100,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Your appointment has been successfully scheduled.',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'We look forward to seeing you on ',
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                '$date',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), // Make the date bold
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                ' at ',
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                '$time',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), // Make the time bold
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                ' at Malabe.',
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Navigate to the HomePage
+              Navigator.of(context).pop();
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xff00B140), // Background color
+            ),
+            child: const Text('Go to Home'),
+          ),
+          SizedBox(height: 40),
+        ],
+      ),
+    );
+  }
+}
